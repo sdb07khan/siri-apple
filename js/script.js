3102def40a460e1b443f5e1d41c9496cad645cdc
sdb07khan <ksaddab@gmail.com>
2023-04-13 17:17:32 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// gsap.from(".heroParaText1", {
+//   duration: 1,
+//   opacity: 1,
+//   y: 100,
+//   scale: 0.5,
+//   scrollTrigger: {
+//     trigger: ".heroParaText1",
+//     start: "top 80%",
+//     // end: "bottom center",
+//     // markers: true,
+//   },
+// });
